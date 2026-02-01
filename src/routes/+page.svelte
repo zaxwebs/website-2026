dfa167f4f9a4a036e3a454f09fec4a2ad106f324
zaxwebs <zifidigital@gmail.com>
2026-02-01 11:49:38 +0530

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button/index.js';
+</script>
+
+<section class="min-h-screen">
+	<div class="mx-auto flex max-w-6xl flex-col items-center justify-center gap-4 text-center">
+		<h1 class="text-3xl md:text-4xl lg:text-6xl">
+			Helping Businesses Excel with Design, Development & Marketing
+		</h1>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec convallis purus purus, et
+			malesuada nunc vestibulum eget.
+		</p>
+		<div class="flex gap-2">
+			<Button variant="outline">Get in Touch</Button>
+			<Button variant="outline">About</Button>
+		</div>
+	</div>
+</section>
