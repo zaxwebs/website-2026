5ab09e4817b0c1792c79f76d96e5896a84b00743
zaxwebs <zifidigital@gmail.com>
2026-02-02 16:45:45 +0530

    updated footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,13 +1,13 @@
 <footer id="footer" class="overflow-hidden bg-background">
-	<img class="mx-auto" src="images/typographic.svg" alt="Zack Webster" />
 	<div class="mx-auto max-w-[1400px] p-4">
-		<div class="mt-20 mb-32 flex flex-col justify-between gap-4 md:flex-row">
+		<div class="mt-4 mb-20 flex flex-col justify-between gap-4 md:flex-row">
 			<a href="https://www.instagram.com/zaxwebs">Instagram</a>
 			<a href="https://www.linkedin.com/in/zackwebs/">LinkedIn</a>
 			<a href="https://dribbble.com/zaxwebs">Dribbble</a>
 			<a href="https://github.com/zaxwebs">GitHub</a>
 			<a href="https://dev.to/zaxwebs">Dev.to</a>
 		</div>
+		<img class="mx-auto mb-20" src="images/typographic.svg" alt="Zack Webster" />
 		<div class="flex flex-col justify-between gap-4 md:flex-row">
 			<div>&copy; 2026 Zack Webster. All rights reserved.</div>
 			<div>
