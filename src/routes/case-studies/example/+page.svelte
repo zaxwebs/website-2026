05578c3a084e376803807cc1fa97fd781f9bc09e
zaxwebs <zifidigital@gmail.com>
2026-02-03 18:18:48 +0530

    updated structure on about and case study

--- src/routes/case-studies/example/+page.svelte
+++ src/routes/case-studies/example/+page.svelte
@@ -2,37 +2,39 @@
 	import ContactSection from '$lib/components/ContactSection.svelte';
 </script>
 
-<section class="space-y-8 px-4 pt-32 pb-32 lg:pt-20">
-	<div
-		class="mx-auto flex max-w-6xl flex-col items-center justify-center gap-4 px-4 text-center md:gap-5"
-	>
-		<h1 class="text-3xl leading-[1.1] md:text-4xl lg:text-6xl">
-			Designing a Scalable Platform for a Global SaaS
-		</h1>
-		<p class="mt-0 max-w-md lg:mt-2">
-			I partner with founders and teams to create digital products that are thoughtful, scalable,
-			and built to drive real results.
-		</p>
-	</div>
-	<div class="mx-auto aspect-[16/9] max-w-[1600px]">
-		<img src="https://placehold.co/1600x900/303030/808080" alt="Case Study" />
-	</div>
-	<div class="mx-auto flex w-full max-w-[1400px] flex-col justify-between gap-4 lg:flex-row">
-		<div>
-			<div class="mb-2 text-sm font-medium tracking-wider uppercase opacity-50">Client</div>
-			<div class="text-xl lg:text-2xl">Acme Inc.</div>
+<section>
+	<div class="space-y-8 px-4 pt-32 pb-32 lg:pt-20">
+		<div
+			class="mx-auto flex max-w-6xl flex-col items-center justify-center gap-4 px-4 text-center md:gap-5"
+		>
+			<h1 class="text-3xl leading-[1.1] md:text-4xl lg:text-6xl">
+				Designing a Scalable Platform for a Global SaaS
+			</h1>
+			<p class="mt-0 max-w-md lg:mt-2">
+				I partner with founders and teams to create digital products that are thoughtful, scalable,
+				and built to drive real results.
+			</p>
 		</div>
-		<div>
-			<div class="mb-2 text-sm font-medium tracking-wider uppercase opacity-50">Scope</div>
-			<div class="text-xl lg:text-2xl">Design. Development. Branding.</div>
+		<div class="mx-auto aspect-[16/9] max-w-[1600px]">
+			<img src="https://placehold.co/1600x900/303030/808080" alt="Case Study" />
 		</div>
-		<div>
-			<div class="mb-2 text-sm font-medium tracking-wider uppercase opacity-50">Timeline</div>
-			<div class="text-xl lg:text-2xl">1 Month</div>
-		</div>
-		<div>
-			<div class="mb-2 text-sm font-medium tracking-wider uppercase opacity-50">Industry</div>
-			<div class="text-xl lg:text-2xl">Enterprise SaaS</div>
+		<div class="mx-auto flex w-full max-w-[1400px] flex-col justify-between gap-4 lg:flex-row">
+			<div>
+				<div class="mb-2 text-sm font-medium tracking-wider uppercase opacity-50">Client</div>
+				<div class="text-xl lg:text-2xl">Acme Inc.</div>
+			</div>
+			<div>
+				<div class="mb-2 text-sm font-medium tracking-wider uppercase opacity-50">Scope</div>
+				<div class="text-xl lg:text-2xl">Design. Development. Branding.</div>
+			</div>
+			<div>
+				<div class="mb-2 text-sm font-medium tracking-wider uppercase opacity-50">Timeline</div>
+				<div class="text-xl lg:text-2xl">1 Month</div>
+			</div>
+			<div>
+				<div class="mb-2 text-sm font-medium tracking-wider uppercase opacity-50">Industry</div>
+				<div class="text-xl lg:text-2xl">Enterprise SaaS</div>
+			</div>
 		</div>
 	</div>
 </section>
